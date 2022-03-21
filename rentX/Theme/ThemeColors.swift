3e6fbbfbb7081207ff1e583c0ee00828aed1e588
Tuba <brunogabrielsd@hotmail.com>
2022-03-20 22:08:12 -0300

    theme: added custom background colors and image assets

---
+++ rentX/Theme/ThemeColors.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeColors.swift
+//  rentX
+//
+//  Created by Bruno Gabriel on 10/03/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ThemeColors {
+    
+    static let BACKGROUND_SECONDARY = Color("bgSecondary");
+    static let BACKGROUND_PRIMARY = Color("bgPrimary");
+    
+}
