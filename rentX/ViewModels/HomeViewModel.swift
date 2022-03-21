6469ec4c78f43b053935963ec1ef07867fe442c8
Tuba <brunogabrielsd@hotmail.com>
2022-03-20 22:26:04 -0300

    feat(HomeView): implemented HomeView and HomeViewModel to display a list of available cars and their data

---
+++ rentX/ViewModels/HomeViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewModel.swift
+//  rentX
+//
+//  Created by Bruno Gabriel on 18/03/22.
+//
+
+import Foundation
+import SwiftUI
+
+class HomeViewModel: ObservableObject {
+    @Published var cars: [CarModel] = []
+    
+    init() {
+        self.getCars()
+    }
+    
+    func getCars() {
+        
+        CarService.shared.getCars { (result) in
+            switch result {
+                case.success(let carsData):
+                    self.cars.append(contentsOf: carsData)
+                    print(carsData)
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
+}
---
+++ rentX/Views/HomeView.swift
@@ -0,0 +1,75 @@
+//
+//  HomeView.swift
+//  rentX
+//
+//  Created by Bruno Gabriel on 10/03/22.
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct HomeView: View {
+    @StateObject var homeViewModel: HomeViewModel = HomeViewModel()
+    
+    var body: some View {
+        VStack(spacing: -44) {
+            HStack {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 140)
+                    .padding(.horizontal, 20)
+                    .padding(.top, 46)
+                
+                Spacer()
+                
+                Text("Total de 12 carros")
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.top, 46)
+            }
+            .frame(maxWidth: .infinity, maxHeight: 140)
+            .background(ThemeColors.BACKGROUND_SECONDARY)
+            .cornerRadius(6, corners: [.bottomLeft, .bottomRight])
+            .ignoresSafeArea(.all)
+            
+            VStack {
+                ScrollView(showsIndicators: false) {
+                    ForEach(homeViewModel.cars) {
+                        car in
+                        CarItemView(car: car)
+                            .padding(.top)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .background(ThemeColors.BACKGROUND_PRIMARY)
+            }
+            .foregroundColor(.red)
+        }
+        .background(.regularMaterial)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+        }
+    }
+}
