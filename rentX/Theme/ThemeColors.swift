8334938207a5512c7b58774c8472a7a45b38ab8b
Tuba <brunogabrielsd@hotmail.com>
2022-03-27 22:19:26 -0300

    theme: added new custom colors

--- rentX/Theme/ThemeColors.swift
+++ rentX/Theme/ThemeColors.swift
@@ -10,7 +10,22 @@ import SwiftUI
 
 struct ThemeColors {
     
+    static let HEADER = Color("header");
+    
     static let BACKGROUND_SECONDARY = Color("bgSecondary");
     static let BACKGROUND_PRIMARY = Color("bgPrimary");
+    
+    static let TEXT = Color("text");
+    static let TEXT_DETAIL = Color("textDetail");
+    static let TITLE = Color("title");
+    
+    static let LINE = Color("line");
+    
+    static let MAIN = Color("main")
+    static let MAIN_LIGHT = Color("mainLight");
+    static let SUCCESS = Color("success");
+    
+    static let SHAPE = Color("shape");
+    static let SHAPE_DARK = Color("shape_dark");
     
 }
